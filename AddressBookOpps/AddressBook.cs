24019f80e91f135ffee8b081ea6ebabe96f6bdfe
pallavijawanjal25 <109516737+pallavijawanjal25@users.noreply.github.com>
2022-08-13 23:51:57 +0530

    Update AddressBook.cs

--- AddressBookOpps/AddressBook.cs
+++ AddressBookOpps/AddressBook.cs
@@ -7,11 +7,30 @@ using System.Threading.Tasks;
 namespace AddressBookOpps
 {
      class AddressBook
-    {
-        public List<Contact> ContactList;
+    { public List<Contact> ContactList;
         public AddressBook()
         {
             this.ContactList = new List<Contact>();
+        }
+        //Add Contact to Address Book
+        public void AddContact(Contact contactObj)
+        {
+            this.ContactList.Add(contactObj);
+        }
+        //Find Contact Object Index By Mobile Number
+        public int FindByPhoneNum(long phoneNumber)
+        {
+            return this.ContactList.FindIndex(contact => contact.PhoneNumber.Equals(phoneNumber));
+        }
+        //Find Contact Object Index By FirstName
+        public int FindByFirstName(string firstName)
+        {
+            return this.ContactList.FindIndex(contact => contact.FirstName.Equals(firstName));
+        }
+        //Delete a Give Contact By Index
+        public void DeleteContact(int index)
+        {
+            this.ContactList.RemoveAt(index);
         }
     }
 }
