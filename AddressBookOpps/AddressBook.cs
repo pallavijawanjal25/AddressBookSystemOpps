0145662d5e04520f3a22c206a3ccc4a9cc49413d
pallavijawanjal <pallavij@gmail.com>
2022-08-13 19:45:11 +0530

    Edit The Contact

--- AddressBookOpps/AddressBook.cs
+++ AddressBookOpps/AddressBook.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace AddressBookOpps
 {
-     class AddressBook
+    class AddressBook
     {
         public List<Contact> ContactList;
         public AddressBook()
         {
             this.ContactList = new List<Contact>();
+        }
+        public void AddContact(Contact contactObj)
+        {
+            this.ContactList.Add(contactObj);
+        }
+        public int FindByPhoneNum(long phoneNumber)
+        {
+            return this.ContactList.FindIndex(contact => contact.PhoneNumber.Equals(phoneNumber));
         }
     }
 }
