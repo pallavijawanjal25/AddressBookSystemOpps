54d6902a50ef197309afbbe3638bafd943d4a15a
pallavijawanjal <pallavij@gmail.com>
2022-08-13 19:15:56 +0530

    Add project files.

---
+++ AddressBookOpps/AddressBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookOpps
+{
+     class AddressBook
+    {
+        public List<Contact> ContactList;
+        public AddressBook()
+        {
+            this.ContactList = new List<Contact>();
+        }
+    }
+}
+
+            
---
+++ AddressBookOpps/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Xml.Linq;
+
+namespace AddressBookOpps
+{
+    class Program
+    {
+     
+
+        public static void Main(String[]Args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook obj = new AddressBook();
+            
+        }
+    }
+
+    }
+
